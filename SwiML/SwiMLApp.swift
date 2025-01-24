d247141dc9d4157d02a3fe5db38eb18d5b18f255
xiaoka520 <caoenming520@icloud.com>
2025-01-24 21:14:34 +0800

    初始化Xcode

---
+++ SwiML/SwiMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiMLApp.swift
+//  SwiML
+//
+//  Created by 华人易居 on 2025/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
